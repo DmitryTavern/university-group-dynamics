1b3509057fd835934f6bab2f18f0d88369f3f8ba
Dmitry Tavern <dogger.work@gmail.com>
2022-04-28 01:35:43 +0300

    [22.04.28]: add abs function

--- 22.04.28/index.c
+++ 22.04.28/index.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int getAbs(int number);
 
 int main()
 {
-  printf("Group Dynamic");
+  printf("Group Dynamic\n");
+
+  printf("getAbs(-1) = %d\n", getAbs(-1));
+}
+
+int getAbs(int number)
+{
+  return abs(number);
 }
