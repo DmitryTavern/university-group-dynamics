040f6f90d72234584fcac73c703b5cbb8ff0cbc8
Dmitry Tavern <dogger.work@gmail.com>
2022-04-28 01:37:26 +0300

    [22.04.28]: add square function

--- 22.04.28/index.c
+++ 22.04.28/index.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 
 int getAbs(int number);
+int getSquare(int number);
 
 int main()
 {
   printf("Group Dynamic\n");
 
   printf("getAbs(-1) = %d\n", getAbs(-1));
+  printf("getSquare(3) = %d\n", getSquare(3));
 }
 
 int getAbs(int number)
 {
   return abs(number);
+}
+
+int getSquare(int number)
+{
+  return number * number;
 }
